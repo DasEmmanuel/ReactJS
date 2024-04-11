52d8ec7f904e105b0563d0a38bb39d76f6c24811
DasEmmanuel <102369709+DasEmmanuel@users.noreply.github.com>
2024-04-11 10:54:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const element = (
+    <div className='bgCtn'>
+      <h1 className='main_heading'>Over Over League</h1>
+    </div>
+  )
+  
+  ReactDOM.render(element, document.getElementById('root'))
+  
